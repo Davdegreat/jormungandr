2334f600435b3db1c55d6cb5d2208bd7eb294cfd
Nicolas Di Prima <nicolas.diprima@iohk.io>
2019-07-25 18:38:25 +0200

    add placeholder code for calling next storage

--- jormungandr/src/main.rs
+++ jormungandr/src/main.rs
@@ -261,7 +261,11 @@ fn bootstrap(initialized_node: InitializedNode) -> Result<BootstrappedNode, star
     let (new_epoch_announcements, new_epoch_notifier) = tokio::sync::mpsc::channel(100);
 
     let blockchain =
+        start_up::load_legacy_blockchain(block0, storage, new_epoch_announcements, &bootstrap_logger)?;
+    /*
+    let (new_blockchain, branch) =
         start_up::load_blockchain(block0, storage, new_epoch_announcements, &bootstrap_logger)?;
+        */
 
     network::bootstrap(&settings.network, blockchain.clone(), &bootstrap_logger);
 
